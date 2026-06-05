2bf14098b603658a0085f52fdebcf1126d64a77a
Faith Daisy Muchira <faidamuchira@gmail.com>
2026-06-05 12:40:09 +0100

    Added Faith's introduction

---
+++ app.py
@@ -0,0 +1,7 @@
+"""
+Team Introductions
+"""
+
+# Faith Daisy Muchira
+# Hobby: Singing and listening to music
+# I enjoy singing and listening to music because it helps me relax, express myself, and stay motivated throughout the day.
