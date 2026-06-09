0c94970fae9231b20fc9a6179f1ae58450447075
Faith Daisy Muchira <faidamuchira@gmail.com>
2026-06-09 12:46:04 +0100

    Create MYSQL server connection utility.

---
+++ database/db_connection.py
@@ -0,0 +1,24 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_server_connection():
+    
+    """Connect to MySQL server only.
+    This is to be used when creating the DB
+    """
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")   
+    )
+    
+if __name__ == "__main__":
+    connection = get_server_connection()
+    
+    if connection.is_connected():
+        print("Connected successfully!")
+        
+        connection.close()
