e6d291661ffa211d05a8ca632de4cfaefaeb7efe
Faith Daisy Muchira <faidamuchira@gmail.com>
2026-06-10 16:00:38 +0100

    Add database connection utility

--- database/db_connection.py
+++ database/db_connection.py
@@ -22,3 +22,24 @@ def get_server_connection():
         print("Connected successfully!")
         
         connection.close()"""
+
+def get_db_connection():
+    """Connect to MySQL database, food_waste_db.
+    This is to be used when creating the tables, querying, inserting data, and updating records.
+    """
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")  
+    )
+    
+"""if __name__ == "__main__":
+    connection = get_db_connection()
+    
+    if connection.is_connected():
+        print("Connected to database successfully!")
+        
+    connection.close()"""
+    
+    
